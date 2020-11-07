4b21021ac240e36ef7e650f9e5db8c890f99cb18
theisen92 <andrew.peter.theisen@gmail.com>
2020-11-06 21:49:37 -0600

    Heroku deploy

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
